566509653c2df864ebecf7185f35b6ef48895937
Jan Buschtöns <buschtoens@gmail.com>
2017-07-19 08:54:54 +0200

    configure babel and eslint for ES6 class attributes

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-collect-helper'
+  name: 'ember-collect-helper',
+
+  init: function() {
+    this._super.init && this._super.init.apply(this, arguments);
+
+    this.options = this.options || {};
+    this.options.babel = this.options.babel || {};
+    this.options.babel.plugins = this.options.babel.plugins || [];
+
+    if (this.options.babel.plugins.indexOf('transform-class-properties') === -1) {
+      this.options.babel.plugins.push('transform-class-properties');
+    }
+  }
 };
